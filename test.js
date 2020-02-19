9ad8eb4e5f5c18a010c91f08a8e0eef6b28e67cc
ola <kveola13@student.westerdals.no>
2020-02-19 22:57:56 +0100

    added cards

--- test.js
+++ test.js
@@ -7,3 +7,26 @@ person['age'] = 27;
 
 console.log(person.name);
 console.log(person.age)
+
+let cards = ['Geralt of Rivia', 'Ogre'];
+console.log(cards[0]);
+
+let archerCard = {
+    name: 'Archer',
+    strength: 2,
+    provision: 5
+};
+
+let bowmanCard = {
+    name: 'Bowman',
+    strength: 3,
+    provision: 4
+};
+
+let soldier = {
+    name: 'Soldier',
+    strength: 5,
+    provision: 6
+};
+
+console.table({archerCard, bowmanCard, soldier})
