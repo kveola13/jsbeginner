1e2a096edef87ea9246db4f2f74c11719a4346c7
ola <kveola13@student.westerdals.no>
2020-02-20 21:20:40 +0100

    Components within compontents

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,19 @@
 import React from "react"
 import ReactDom from "react-dom"
 import MyInfo from "./components/MyInfo"
+import Footer from "./components/Footer"
 
 function ReactApp(){
     return (
+      <div>
         <ul>
             <li>Geralt</li>
             <li>Jaskier</li>
             <li>Yarpen</li>
-        </ul>)
+        </ul>
+        <Footer/>
+      </div>
+      )
 }
 
 ReactDom.render(<ReactApp/>, document.getElementById("root"))
