0c88071e07ea6f8d5c0e700d09128e37bb130718
ola <kveola13@student.westerdals.no>
2020-02-22 12:30:15 +0100

    Better structure

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDom from "react-dom"
-import WhoSaid from "./components/WhoSaid"
+import WhoSaid from "./components/Quotes/WhoSaid"
 
 function ReactApp(){
     return (
--- react-app/src/components/Basics/MainComponent.js
+++ react-app/src/components/Basics/MainComponent.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Header from "./Header"
 import Footer from "./Footer"
-import MoreComplexApp from "./MoreComplexApp"
+import MoreComplexApp from "../MoreComplexApp"
 
 function MainComponent() {
     return(
