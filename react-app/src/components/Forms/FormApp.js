d718d501f851aa1ad5ddbbe5aae956d420243930
ola <kveola13@student.westerdals.no>
2020-02-25 23:48:16 +0100

    Went through all the forms for React

--- react-app/src/components/Forms/FormApp.js
+++ react-app/src/components/Forms/FormApp.js
@@ -5,13 +5,19 @@ class FormApp extends Component {
         super()
         this.state = {
             enteredText: "",
-            secondText: ""
+            secondText: "",
+            killingMonsters: true,
+            witcher: "",
+            potion: ""
         }
         this.handleTextChange = this.handleTextChange.bind(this)
     }
 
     handleTextChange(event) {
-        const {name, value} = event.target
+        console.log(this.state.killingMonsters)
+        const {name, value, type, checked} = event.target
+        
+        type === "checkbox"? this.setState({[name]:checked}) :
         this.setState({
             [name]: value
         })
@@ -20,19 +26,76 @@ class FormApp extends Component {
     render(){
         return(
             <form>
-                <input type="text" 
-                value={this.state.enteredText} 
-                name="enteredText" 
-                placeholder="Enter text" 
-                onChange={this.handleTextChange}/>
+                <label>
+                    <input 
+                        type="text" 
+                        value={this.state.enteredText} 
+                        name="enteredText" 
+                        placeholder="Enter text" 
+                        onChange={this.handleTextChange}
+                    />
+                    <br/>
+                    <input 
+                        type="text" 
+                        value={this.state.secondText}
+                        name="secondText" 
+                        placeholder="Enter second text" 
+                        onChange={this.handleTextChange}
+                    />
+                    <br/>
+                </label>
                 
-                <input type="text" 
-                value={this.state.secondText}
-                name="secondText" 
-                placeholder="Enter second text" 
-                onChange={this.handleTextChange}/>
-
-                <h2>{this.state.enteredText}{this.state.secondText}</h2>
+                
+                <label>
+                    <textarea value={"Geralt is bae"}
+                    onChange={this.handleTextChange}/>
+                </label>
+                
+                <br/>
+                <label>
+                    <input 
+                        type="checkbox" 
+                        name="killingMonsters"
+                        checked={this.state.killingMonsters}
+                        onChange={this.handleTextChange}
+                    />
+                </label>
+                <br/>
+                <label>
+                    <input 
+                        type="radio" 
+                        name="witcher"
+                        value="Yes"
+                        checked={this.state.witcher==="Yes"}
+                        onChange={this.handleTextChange}
+                    /> Yes
+                </label>
+                <label>
+                    <input 
+                        type="radio" 
+                        name="witcher"
+                        value="No"
+                        checked={this.state.witcher==="No"}
+                        onChange={this.handleTextChange}
+                    /> No
+                </label>
+                <br/>
+                <label>
+                    <select 
+                        value={this.state.potion}
+                        onChange={this.handleTextChange}
+                        name="potion">
+                        <option value="Archgriffin Decoction">Archgriffin Decoction</option>
+                        <option value="Tawny Owl">Tawny Owl</option>
+                        <option value="Swallow">Swallow</option>
+                        <option value="Green Oriole">Green Oriole</option>
+                        <option value="Cat">Cat</option>
+                    </select>
+                </label>
+                
+                <br/>
+                <h3>{this.state.enteredText} is a witcher? {this.state.witcher}</h3>
+                <p>Chosen decoction is {this.state.potion}</p>
             </form>
         )
     }
